009d7121c887dff6cf975ce84460ebf96b9004b2
divineLush <alvipath@yahoo.com>
2021-07-13 07:14:29 -0400

    mapping mutations

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,12 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    isAuthModalVisible: false,
   },
   mutations: {
+    toggleAuthModal: (state) => {
+      state.isAuthModalVisible = !state.isAuthModalVisible;
+    },
   },
   actions: {
   },
