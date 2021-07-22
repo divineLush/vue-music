23c96eea5682dc8cc42c93442189d721b6c3fdf0
divineLush <alvipath@yahoo.com>
2021-07-22 21:10:46 +0700

    validating the form

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -86,62 +86,79 @@
           </form>
           <!-- Registration Form -->
           <!-- validation-schema attribute allows to outsource the rules into an object -->
-          <vee-form v-if="tab === 'register'" :validation-schema="schema">
+          <vee-form
+            v-if="tab === 'register'"
+            :validation-schema="schema"
+            @submit="register"
+          >
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
               <vee-field type="text" name="name"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name" :rules="'required'" />
+                placeholder="Enter Name" />
               <ErrorMessage class="text-red-600" name="name" />
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input type="email"
+              <vee-field type="email" name="email"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Email" />
+              <ErrorMessage class="text-red-600" name="email" />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input type="number"
+              <vee-field type="number" name="age"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded" />
+              <ErrorMessage class="text-red-600" name="age" />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input type="password"
+              <vee-field type="password" name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Password" />
+              <ErrorMessage class="text-red-600" name="password" />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input type="password"
+              <vee-field type="password" name="confirm_password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Confirm Password" />
+              <ErrorMessage class="text-red-600" name="confirm_password" />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <!-- as="select" is needed to generate the select tag -->
+              <vee-field as="select" name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded">
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Imaginationland">Imaginationland</option>
+              </vee-field>
+              <ErrorMessage class="text-red-600" name="country" />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <vee-field
+                type="checkbox"
+                name="tos"
+                value="1"
+                class="w-4 h-4 float-left -ml-6 mt-1 rounded"
+              />
               <label class="inline-block">Accept terms of service</label>
+              <ErrorMessage class="text-red-600" name="tos" />
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
@@ -165,19 +182,23 @@ export default {
     return {
       tab: 'login',
       schema: {
-        name: 'required',
-        email: '',
-        age: '',
-        password: '',
-        confirmedPassword: '',
-        country: '',
-        tos: '',
+        name: 'required|min:3|max:100|alpha_spaces',
+        email: 'required|min:3|max:100|email',
+        age: 'required|min_value:18|max_value:120',
+        password: 'required|min:3|max:100',
+        // @password must match a name we're checking against
+        confirm_password: 'confirmed:@password',
+        country: 'required|excluded:Imaginationland',
+        tos: 'required',
       },
     };
   },
 
   methods: {
     ...mapMutations(['toggleAuthModal']),
+    register(values) {
+      console.log(values);
+    },
   },
 
   computed: {
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,20 +1,39 @@
 import {
-  Form as VeeForm,
-  Field as VeeField,
   defineRule,
   ErrorMessage,
+  Field as VeeField,
+  Form as VeeForm,
 } from 'vee-validate';
-import { required } from '@vee-validate/rules';
+import {
+  required,
+  min,
+  max,
+  email,
+  confirmed,
+  alpha_spaces as alphaSpaces,
+  min_value as minVal,
+  max_value as maxVal,
+  not_one_of as excluded,
+} from '@vee-validate/rules';
 
 // plugins are objects with a method called install
 export default {
-  // "app" is a reference to the Vue app
-  // "options" is additional data passed from the Vue instance to the plugin
+  // 'app' is a reference to the Vue app
+  // 'options' is additional data passed from the Vue instance to the plugin
   install(app) {
     app.component('VeeForm', VeeForm);
     app.component('VeeField', VeeField);
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('min', min);
+    defineRule('max', max);
+    // alpha_spaces only allows alphabetic characters
+    defineRule('alpha_spaces', alphaSpaces);
+    defineRule('email', email);
+    defineRule('min_value', minVal);
+    defineRule('max_value', maxVal);
+    defineRule('confirmed', confirmed);
+    defineRule('excluded', excluded);
   },
 };
