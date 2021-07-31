84be47db778c67f8c6bb1b8461b54feba4bf16d1
divineLush <alvipath@yahoo.com>
2021-07-31 23:02:46 +0700

    setting firebase usernames as uids

--- src/store/index.js
+++ src/store/index.js
@@ -23,13 +23,21 @@ export default createStore({
       age,
       country,
     }) {
-      await auth.createUserWithEmailAndPassword(email, password);
-      await usersCollection.add({
-        name,
-        email,
-        age,
-        country,
-      });
+      const userCredentials = await auth.createUserWithEmailAndPassword(email, password);
+
+      await usersCollection
+        // .doc() adds document to the database and returns a document
+        .doc(userCredentials.user.uid)
+        // .set() adds/modifies properties in a document
+        .set({
+          name,
+          email,
+          age,
+          country,
+        });
+
+      // every registered user comes with a profile
+      await userCredentials.user.updateProfile({ displayName: name });
 
       commit('toggleAuth');
     },
