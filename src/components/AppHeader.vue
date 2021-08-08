771f722ba1fce5ab092c8c47f32def6557eeed9c
divineLush <alvipath@yahoo.com>
2021-08-09 00:05:12 +0700

    manage section & active links customizations

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,12 +2,27 @@
   <header id="header" class="bg-gray-700">
     <nav class="container mx-auto flex justify-start items-center py-5 px-4">
       <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
+      <router-link
+        class="text-white font-bold uppercase text-2xl mr-4"
+        to="/"
+        exact-active-class="no-active"
+      >
+        Music
+      </router-link>
 
       <div class="flex flex-grow items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
+          <li>
+            <router-link
+              v-if="isUserLoggedIn"
+              class="px-2 text-white"
+              to="/manage"
+            >
+              Manage
+            </router-link>
+          </li>
           <li v-if="!isUserLoggedIn">
             <a
               class="px-2 text-white"
@@ -19,7 +34,7 @@
           </li>
           <template v-else>
             <li>
-              <a class="px-2 text-white" href="#">Manage</a>
+              <router-link class="px-2 text-white" to="#">Manage</router-link>
             </li>
             <li>
               <a
